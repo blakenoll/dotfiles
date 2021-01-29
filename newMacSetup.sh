4c32e211bbdca6c9b3aa16f1213dd02338f87a35
Blake Noll <nollblake@gmail.com>
2021-01-28 23:38:03 -0800

    inital commit

---
+++ newMacSetup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# install developer tools
+xcode-select —install
+# install homebrew
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+# install git
+brew install git
+# install node
+brew install node
+# install deno
+brew install deno
+# install rectangle
+brew install --cask rectangle
+# install brave
+brew install --cask brave-browser
+# install vs code 
+brew install --cask visual-studio-code
+# install iTerm2
+brew install --cask iterm2
+# install postman
+brew install --cask postman
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# download jetbrains mono
+curl https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip --output jetbrainsMono.zip
+unzip jetbrainsMono.zip
